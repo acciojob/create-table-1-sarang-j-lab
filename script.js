93383c1e0ca7b352b1425bfc44f2757e0ace55ff
Sarang <149196971+sarang-j-lab@users.noreply.github.com>
2025-10-08 10:52:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
+	
 function insert_Row() {
     //Write your code here
-  
-  
+	const table = document.querySelector("table");
+	const row = table.insertRow(0);
+	const col1 = row.insertCell(0)
+	const col2 = row.insertCell(1)
+
+	col1.innerHTML = "New Cell1"
+	col2.innerHTML = "New Cell2"
+	table.prepand(tr);
 }
